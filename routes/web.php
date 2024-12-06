76921a810b61c857cd60dd199cdaecc7298f56d2
Habsyi2512 <githabsyi@gmail.com>
2024-12-07 04:44:07 +0700

    Simpan perubahan lokal pada web.php

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard/tambah-penduduk', function(){
+        return Inertia::render('Form/AddPenduduk');
+    });
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
