fe92e8f5fef44a7f99aad8778b7d5695a28b8e26
Habsyi2512 <githabsyi@gmail.com>
2024-12-16 00:47:51 +0700

    `Refactor DebugController and Debug page: remove search functionality and update Debug page to render Debug/Debug`

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -10,33 +10,6 @@ use Inertia\Inertia;
 class DebugController extends Controller
 {
     public function index(){
-        return Inertia::render('Debug');
-    }
-
-    public function search(Request $request){
-        $query = $request->input('q');
-
-        // Query data desa/kelurahan yang berada di provinsi Kepulauan Riau
-        $results = Village::where('name', 'like', '%' . $query . '%')
-            ->whereHas('district.regency.province', function ($q) {
-                $q->where('id', 21); // Batasi hanya pada provinsi dengan ID 21 (Kepulauan Riau)
-            })
-            ->with(['district.regency.province']) // Load relasi sampai province
-            ->limit(10)
-            ->get()
-            ->map(function ($village) {
-                return [
-                    'id' => $village->id,
-                    'name' => $village->name,
-                    'district_id' => $village->district_id,
-                    'district_name' => $village->district->name ?? null, // Nama kecamatan
-                    'regency_id' => $village->district->regency_id ?? null,
-                    'regency_name' => $village->district->regency->name ?? null, // Nama kabupaten
-                    'province_id' => $village->district->regency->province_id ?? null,
-                    'province_name' => $village->district->regency->province->name ?? null, // Nama provinsi
-                ];
-            });
-
-        return response()->json($results);
+        return Inertia::render('Debug/Debug');
     }
 }
